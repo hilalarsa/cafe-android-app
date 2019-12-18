0dcdee4cecab6c36c2f5c3e5a257eb37a9537ef8
anitasari15 <anitasari.as250@gmail.com>
2019-12-18 22:35:31 +0700

    menambahkan cafe

--- app/src/main/java/com/example/cafemalangv20/ListCafe.java
+++ app/src/main/java/com/example/cafemalangv20/ListCafe.java
@@ -31,12 +31,14 @@ public class ListCafe extends AppCompatActivity {
     EditText txtNamaCafe, txtLokasi, txtDeskripsi;
     List<Menu> Cafes;
     ListView listViewCafe;
+    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_cafe);
         initViews();
+        initListeners();
 
         mServiceApi = RetrofitServer.getClient().create(ServiceApi.class);
         Call<List<Cafe>> cafeCall = mServiceApi.getCafe();
@@ -65,13 +67,25 @@ public class ListCafe extends AppCompatActivity {
                 Log.d("TAG",t.toString());
             }
         });
-
-
-
     }
 
     private void initViews() {
+
         listViewCafe = findViewById(R.id.listViewCafe);
+        txtNamaCafe = findViewById(R.id.updateTextCafe);
+        txtLokasi = findViewById(R.id.updateTextLokasi);
+        txtDeskripsi = findViewById(R.id.txtDeskripsi);
+
+        btnAdd = findViewById(R.id.btn_add);
+    }
+
+    private void initListeners() {
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addCafe();
+            }
+        });
     }
 
     private void CallUpdateAndDeleteDialog(final String id_cafe, final String nama_cafe, final String lokasi, final String deskripsi, String id_user){
@@ -82,16 +96,14 @@ public class ListCafe extends AppCompatActivity {
         dialogBuilder.setView(dialogView);
 
         //Access Dialog views for Menu
-        txtNamaCafe = dialogView.findViewById(R.id.updateTextCafe);
-        txtLokasi = dialogView.findViewById(R.id.updateTextLokasi);
-        txtDeskripsi = dialogView.findViewById(R.id.txtDeskripsi);
+        final EditText txtNamaCafe = dialogView.findViewById(R.id.updateTextCafe);
+        final EditText txtLokasi = dialogView.findViewById(R.id.updateTextLokasi);
+        final EditText txtDeskripsi = dialogView.findViewById(R.id.txtDeskripsi);
 
 
         txtNamaCafe.setText(nama_cafe);
         txtLokasi.setText(lokasi);
         txtDeskripsi.setText(deskripsi);
-
-
 
         final Button btnUpdate = dialogView.findViewById(R.id.btnUpdate);
         final Button btnDelete = dialogView.findViewById(R.id.btnDelete);
@@ -129,13 +141,85 @@ public class ListCafe extends AppCompatActivity {
         });
     }
 
+    public boolean addCafe(){
+        String nama_cafe = txtNamaCafe.getText().toString().trim();
+        String lokasi = txtLokasi.getText().toString().trim();
+        String deskripsi = txtDeskripsi.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(nama_cafe)) {
+            if (!TextUtils.isEmpty(lokasi)) {
+                if (!TextUtils.isEmpty(deskripsi)) {
+                        //do add here bruh
+                        Call<Cafe> cafeCall = mServiceApi.postCafe(nama_cafe, lokasi, deskripsi, "1");
+                        cafeCall.enqueue(new Callback<Cafe>() {
+                            @Override
+                            public void onResponse(Call<Cafe> call, Response<Cafe> response) {
+                                Log.d("TAG", "Post success");
+                                Log.d("TAG", response.body().toString());
+                                txtNamaCafe.setText("");
+                                txtLokasi.setText("");
+                                txtDeskripsi.setText("");
+                                Toast.makeText(getApplicationContext(), "Cafe added", Toast.LENGTH_LONG).show();
+                            }
+
+                            @Override
+                            public void onFailure(Call<Cafe> call, Throwable t) {
+                                Log.d("TAG", "Post failure");
+                            }
+                        });
+                    } else {
+                        Toast.makeText(this, "Masukkan Nama Cafe", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(this, "Masukkan Lokasi", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                Toast.makeText(this, "Masukkan Deskripsi", Toast.LENGTH_LONG).show();
+        }
+
+        Toast.makeText(getApplicationContext(), "Cafe Updated", Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+
     public boolean updateCafe(String id_cafe, String nama_cafe, String lokasi, String deskripsi){
+        Call<Cafe> cafeCall = mServiceApi.putCafe(nama_cafe, lokasi, deskripsi,  "1");
+        cafeCall.enqueue(new Callback<Cafe>() {
+            @Override
+            public void onResponse(Call<Cafe> call, Response<Cafe> response) {
+                Log.d("TAG", "Put success");
+                Log.d("TAG", response.body().toString());
+                txtNamaCafe.setText("");
+                txtLokasi.setText("");
+                txtDeskripsi.setText("");
+            }
+
+            @Override
+            public void onFailure(Call<Cafe> call, Throwable t) {
+                Log.d("TAG", "Put failure");
+                Log.d("TAG", t.toString());
+            }
+        });
         Toast.makeText(getApplicationContext(), "Cafe Updated", Toast.LENGTH_LONG).show();
         return true;
     }
 
     public boolean deleteCafe(String id_cafe){
-        Toast.makeText(getApplicationContext(), "Cafe Deleted", Toast.LENGTH_LONG).show();
+        Call<Cafe> cafeCall = mServiceApi.deleteCafe(id_cafe);
+        cafeCall.enqueue(new Callback<Cafe>() {
+            @Override
+            public void onResponse(Call<Cafe> call, Response<Cafe> response) {
+                Log.d("TAG", "Put success");
+                Log.d("TAG", response.body().toString());
+                Toast.makeText(getApplicationContext(), "Cafe Deleted", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFailure(Call<Cafe> call, Throwable t) {
+                Log.d("TAG", "Delete failure");
+                Log.d("TAG", t.toString());
+            }
+        });
         return true;
     }
 
